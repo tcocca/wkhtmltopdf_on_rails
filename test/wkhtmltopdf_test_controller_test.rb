89e45b9199622d0795b897d9a6077a6de50e2264
Tom Cocca <tom.cocca@gmail.com>
2009-11-03 20:39:23 -0500

    test the ensure the pdf is deleted when the :delete_generated_pdf  => true option is supplied

--- test/wkhtmltopdf_test_controller_test.rb
+++ test/wkhtmltopdf_test_controller_test.rb
@@ -18,14 +18,26 @@ class WkhtmltopdfTestController < ActionController::Base
     end
   end
   
+  def test_source_with_delete
+    respond_to do |format|
+      format.pdf do
+        render_to_pdf({
+          :pdf       => "source", 
+          :file_path => File.join(File.dirname(__FILE__), 'pdfs'), 
+          :source    => "http://www.google.com/",
+          :delete_generated_pdf  => true
+        })
+      end
+    end
+  end
+  
   def test_html
     respond_to do |format|
       format.pdf do
         render_to_pdf({
           :pdf                   => "html", 
           :file_path             => File.join(File.dirname(__FILE__), 'pdfs'), 
-          :template              => File.join(File.dirname(__FILE__), 'fixtures', 'wkhtmltopdf.html'),
-          :delete_generated_pdf  => true
+          :template              => File.join(File.dirname(__FILE__), 'fixtures', 'wkhtmltopdf.html')
         })
       end
     end
@@ -35,7 +47,7 @@ end
 ActionController::Routing::Routes.draw do |map|
   map.resources :pdfs,
     :controller => 'wkhtmltopdf_test',
-    :collection => [:test_source, :test_html]
+    :collection => [:test_source, :test_source_with_delete, :test_html]
 end
 
 class WkhtmltopdfTestControllerTest < ActionController::TestCase
@@ -49,6 +61,21 @@ class WkhtmltopdfTestControllerTest < ActionController::TestCase
     
     should_respond_with :success
     should_respond_with_content_type "application/pdf"
+  end
+  
+  context "on GET to pdf with deleting the pdf after generation" do
+    setup do
+      get :test_source_with_delete, :format => "pdf"
+    end
+    
+    subject { @controller }
+    
+    should_respond_with :success
+    should_respond_with_content_type "application/pdf"
+    
+    should "delete the pdf after sending the file" do
+      assert !File.exists?(File.join(File.dirname(__FILE__), 'pdfs', 'source.pdf'))
+    end
   end
   
   context "on GET to pdf from a template" do
